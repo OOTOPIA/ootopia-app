59abe37de7b441a6dfc1aeaf763d32801976775c
Andrew Dias <andrew.1995@outlook.com>
2021-02-18 15:59:26 -0300

    wip(camera): add player camera

--- lib/main.dart
+++ lib/main.dart
@@ -5,41 +5,41 @@ import './screens/camera_screen/camera_screen.dart';
 
 import 'package:camera/camera.dart';
 
-// main() => runApp(ExpensesApp());
+main() => runApp(ExpensesApp());
 
-// class ExpensesApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       // home: TimelinePage(),
-//       home: CameraExampleHome(),
-//     );
-//   }
-// }
-
-class CameraApp extends StatelessWidget {
-  List<CameraDescription> cameras = [];
-
-  @override
-  Future<void> initState() async {
-    try {
-      WidgetsFlutterBinding.ensureInitialized();
-      cameras = await availableCameras();
-    } on CameraException catch (e) {
-      logError(e.code, e.description);
-    }
-  }
-
+class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: CameraExampleHome(cameras: this.cameras),
+      // home: TimelinePage(),
+      home: CameraScreen(),
     );
   }
 }
 
-Future<void> main() async {
-  // Fetch the available cameras before initializing the app.
+// class CameraApp extends StatelessWidget {
+//   List<CameraDescription> cameras = [];
 
-  runApp(CameraApp());
-}
+//   @override
+//   Future<void> initState() async {
+//     try {
+//       WidgetsFlutterBinding.ensureInitialized();
+//       cameras = await availableCameras();
+//     } on CameraException catch (e) {
+//       logError(e.code, e.description);
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: CameraExampleHome(cameras: this.cameras),
+//     );
+//   }
+// }
+
+// Future<void> main() async {
+//   // Fetch the available cameras before initializing the app.
+
+//   runApp(CameraApp());
+// }
