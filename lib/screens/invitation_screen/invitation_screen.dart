c247833432fd98eacb55a29235db6b2ead474f02
anderson.barros <anderson.barros@vsaude.com.br>
2021-08-25 12:53:22 -0300

    add plugin social media

--- lib/screens/invitation_screen/invitation_screen.dart
+++ lib/screens/invitation_screen/invitation_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:ootopia_app/screens/auth/auth_store.dart';
 import 'package:ootopia_app/shared/page-enum.dart' as PageRoute;
+import 'package:provider/provider.dart';
 
 class InvitationScreen extends StatefulWidget {
   const InvitationScreen({Key? key}) : super(key: key);
@@ -12,6 +14,8 @@ class InvitationScreen extends StatefulWidget {
 class _InvitationScreenState extends State<InvitationScreen> {
   @override
   Widget build(BuildContext context) {
+    AuthStore authStore = Provider.of<AuthStore>(context);
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -74,54 +78,65 @@ class _InvitationScreenState extends State<InvitationScreen> {
                   SizedBox(
                     height: 24,
                   ),
-                  Divider(
-                    thickness: 2,
-                    color: Colors.grey,
-                  ),
-                  SizedBox(
-                    height: 24,
-                  ),
-                  Row(
-                    children: [
-                      Flexible(
-                        flex: 3,
-                        child: ListTile(
-                          title: Padding(
-                            padding: const EdgeInsets.only(bottom: 8.0),
-                            child: Text(
-                              AppLocalizations.of(context)!.sowerInvitationCode,
-                              style: TextStyle(
-                                color: Color(0xff018F9C),
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              ),
+                  authStore.currentUser!.badges!.length > 2
+                      ? Column(
+                          children: [
+                            Divider(
+                              thickness: 2,
+                              color: Colors.grey,
                             ),
-                          ),
-                          subtitle: Text(AppLocalizations.of(context)!
-                              .sendSowerInvitationCode),
-                        ),
-                      ),
-                      Column(
-                        children: [
-                          Chip(
-                            label: Text(
-                              'ADCE963',
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 16),
+                            SizedBox(
+                              height: 24,
                             ),
-                            backgroundColor: Color(0xff018F9C),
-                            padding: EdgeInsets.only(left: 25, right: 25),
-                          ),
-                          Text(
-                            AppLocalizations.of(context)!.shareThisCode,
-                            softWrap: true,
-                            style: TextStyle(
-                                color: Color(0xff018F9C), fontSize: 12),
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
+                            Row(
+                              children: [
+                                Flexible(
+                                  flex: 3,
+                                  child: ListTile(
+                                    title: Padding(
+                                      padding:
+                                          const EdgeInsets.only(bottom: 8.0),
+                                      child: Text(
+                                        AppLocalizations.of(context)!
+                                            .sowerInvitationCode,
+                                        style: TextStyle(
+                                          color: Color(0xff018F9C),
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                    subtitle: Text(AppLocalizations.of(context)!
+                                        .sendSowerInvitationCode),
+                                  ),
+                                ),
+                                Column(
+                                  children: [
+                                    Chip(
+                                      label: Text(
+                                        'ADCE963',
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 16),
+                                      ),
+                                      backgroundColor: Color(0xff018F9C),
+                                      padding:
+                                          EdgeInsets.only(left: 25, right: 25),
+                                    ),
+                                    Text(
+                                      AppLocalizations.of(context)!
+                                          .shareThisCode,
+                                      softWrap: true,
+                                      style: TextStyle(
+                                          color: Color(0xff018F9C),
+                                          fontSize: 12),
+                                    )
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ],
+                        )
+                      : Container(),
                   Divider(
                     thickness: 2,
                     color: Colors.grey,
@@ -152,14 +167,17 @@ class _InvitationScreenState extends State<InvitationScreen> {
                       ),
                       Column(
                         children: [
-                          Chip(
-                            label: Text(
-                              'ADCE963',
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 16),
+                          InkWell(
+                            onTap: () async {},
+                            child: Chip(
+                              label: Text(
+                                'ADCE963',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 16),
+                              ),
+                              backgroundColor: Color(0xff003694),
+                              padding: EdgeInsets.only(left: 25, right: 25),
                             ),
-                            backgroundColor: Color(0xff003694),
-                            padding: EdgeInsets.only(left: 25, right: 25),
                           ),
                           Text(
                             AppLocalizations.of(context)!.shareThisCode,
@@ -170,28 +188,25 @@ class _InvitationScreenState extends State<InvitationScreen> {
                         ],
                       ),
                     ],
-                  )
+                  ),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: IconButton(
+                      iconSize: 40,
+                      onPressed: () {
+                        Navigator.of(context).pushNamed(
+                          PageRoute.Page.chatWithUsersScreen.route,
+                        );
+                      },
+                      icon: Image.asset('assets/icons/crisp_icon.png'),
+                    ),
+                  ),
                 ],
               ),
             )
           ],
         ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-      floatingActionButton: IconButton(
-        iconSize: 40,
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-        onPressed: () {
-          Navigator.of(context).pushNamed(
-            PageRoute.Page.chatWithUsersScreen.route,
-          );
-        },
-        icon: Image.asset('assets/icons/crisp_icon.png'),
-      ),
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(icon: Icon(Icons.add), label: ''),
-        BottomNavigationBarItem(icon: Icon(Icons.add), label: ''),
-      ]),
     );
   }
 }
