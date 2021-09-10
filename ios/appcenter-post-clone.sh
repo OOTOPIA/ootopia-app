07d0847b22ddabbe77a3c0e7e83f2bc070207526
Danillo <danillo@devmagic.com.br>
2021-09-10 20:39:24 -0300

    chore(appcenter-post-clone): switched pub upgrade to pub get to follow locked the dependencies

--- ios/appcenter-post-clone.sh
+++ ios/appcenter-post-clone.sh
@@ -16,14 +16,5 @@ flutter doctor
 
 echo "Installed flutter to `pwd`/flutter"
 
-# echo "API_URL=${API_URL} 
-# AMPLITUDE_KEY=${AMPLITUDE_KEY}" > .env
-
-cat > .env <<EOL
-API_URL=${API_URL}
-AMPLITUDE_KEY=${AMPLITUDE_KEY}
-EOL
-cat .env
-
-flutter pub upgrade
+flutter pub get
 flutter build ios --no-sound-null-safety --release --no-codesign
