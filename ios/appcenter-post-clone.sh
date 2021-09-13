99fd3dcc577de23bd7d016c0863f5c3fa3ea7fdd
danillosantos <danillo@devmagic.com.br>
2021-09-13 12:07:47 -0300

    Update appcenter-post-clone.sh

--- ios/appcenter-post-clone.sh
+++ ios/appcenter-post-clone.sh
@@ -16,5 +16,12 @@ flutter doctor
 
 echo "Installed flutter to `pwd`/flutter"
 
+cat > .env <<EOL
+API_URL=${API_URL}
+AMPLITUDE_KEY=${AMPLITUDE_KEY}
+CRISP_WEBSITE_ID=${CRISP_WEBSITE_ID}
+EOL
+cat .env
+
 flutter pub get
 flutter build ios --no-sound-null-safety --release --no-codesign
