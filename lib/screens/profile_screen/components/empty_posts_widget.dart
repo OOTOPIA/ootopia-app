18bf813ece65247d02fddb083315e776ca70d5ce
Andrew Dias <andrew.1995@outlook.com>
2021-09-15 12:34:02 -0300

    fix: fiz empty post

--- lib/screens/profile_screen/components/empty_posts_widget.dart
+++ lib/screens/profile_screen/components/empty_posts_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class EmptyPostsWidget extends StatefulWidget {
   EmptyPostsWidget({Key? key}) : super(key: key);
@@ -23,7 +24,7 @@ class _EmptyPostsWidgetState extends State<EmptyPostsWidget> {
             height: 16,
           ),
           Text(
-            "This user has no publications yet",
+            AppLocalizations.of(context)!.thisUserHasNoPublicationsYet,
             style: TextStyle(
               color: Color(0xff707070).withOpacity(.5),
               fontSize: 16,
