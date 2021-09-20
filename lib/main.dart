959e55950c1b6235fb1e478815dd2eaa40bda315
Claudio Oliveira <claudio_pg-sp@hotmail.com>
2021-09-20 13:39:43 -0300

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -76,28 +76,22 @@ Future main() async {
     child: new ExpensesApp(),
   );
 
-  runApp(
-    GlobalConstants(
-      child: configuredApp,
+  await SentryFlutter.init(
+    (options) {
+      options.dsn =
+          'https://5a5d45bd48bd4a159f2b00f343408ab9@o566687.ingest.sentry.io/5743561';
+      options.debug = false;
+      options.environment = "staging";
+      options.attachStacktrace = true;
+      options.diagnosticLevel = SentryLevel.error;
+      options.enableAutoSessionTracking = true;
+    },
+    appRunner: () => runApp(
+      GlobalConstants(
+        child: configuredApp,
+      ),
     ),
   );
-
-  // await SentryFlutter.init(
-  //   (options) {
-  //     options.dsn =
-  //         'https://5a5d45bd48bd4a159f2b00f343408ab9@o566687.ingest.sentry.io/5743561';
-  //     options.debug = false;
-  //     options.environment = "staging";
-  //     options.attachStacktrace = true;
-  //     options.diagnosticLevel = SentryLevel.error;
-  //     options.enableAutoSessionTracking = true;
-  //   },
-  //   appRunner: () => runApp(
-  //     GlobalConstants(
-  //       child: configuredApp,
-  //     ),
-  //   ),
-  // );
 }
 
 class ExpensesApp extends StatefulWidget {
