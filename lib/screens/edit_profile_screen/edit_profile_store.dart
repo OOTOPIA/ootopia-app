3e57ed278f3b0d1ed1cb9cb51a418c404da31488
anderson.barros <anderson.barros@vsaude.com.br>
2021-09-27 09:35:11 -0300

    fix: errors in ios

--- lib/screens/edit_profile_screen/edit_profile_store.dart
+++ lib/screens/edit_profile_screen/edit_profile_store.dart
@@ -6,7 +6,6 @@ import 'package:intl_phone_number_input/intl_phone_number_input.dart';
 import 'package:mobx/mobx.dart';
 import 'package:ootopia_app/data/models/users/user_model.dart';
 import 'package:ootopia_app/data/repositories/user_repository.dart';
-import 'package:ootopia_app/screens/home/components/page_view_controller.dart';
 
 part 'edit_profile_store.g.dart';
 
