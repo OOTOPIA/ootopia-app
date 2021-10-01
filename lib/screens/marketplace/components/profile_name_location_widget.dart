1c243116544eb7fd02097b560029b883f7b1e609
Gracyele <gracyelesantossj@gmail.com>
2021-10-01 17:52:45 -0300

    feat: create ProfileNameLocation widget

---
+++ lib/screens/marketplace/components/profile_name_location_widget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class ProfileNameLocationWidget extends StatelessWidget {
+  final String profileImageUrl, profileName, location;
+  const ProfileNameLocationWidget(
+      {required this.profileImageUrl,
+      required this.profileName,
+      required this.location});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 26, top: 16, bottom: 16.5, right: 26),
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 18,
+            backgroundImage: NetworkImage(profileImageUrl),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 19),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  profileName,
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  location,
+                  style: TextStyle(fontSize: 12),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
