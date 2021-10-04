4d6a4df697bb501edaa16ad72b1ea2941482e223
Caio Jesus <caio.nascimento@dcomp.ufs.br>
2021-10-04 14:52:57 -0300

    feat: add dashed border in CreateOffer button

--- lib/screens/marketplace/components/create_offer_button_widget.dart
+++ lib/screens/marketplace/components/create_offer_button_widget.dart
@@ -1,3 +1,4 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -14,29 +15,35 @@ class CreateOfferButtonWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () => onTap(),
-      child: Container(
-        alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SvgPicture.asset(
-              'assets/icons/thin_add_icon.svg',
-            ),
-            SizedBox(height: 10),
-            Text(
-              AppLocalizations.of(context)!.createYourOffer,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: LightColors.grey,
+      child: DottedBorder(
+        strokeCap: StrokeCap.square,
+        dashPattern: [10, 10],
+        borderType: BorderType.RRect,
+        radius: Radius.circular(12),
+        child: Container(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                'assets/icons/thin_add_icon.svg',
               ),
-            ),
-          ],
-        ),
-        height: 171,
-        width: 171,
-        decoration: BoxDecoration(
-          color: LightColors.grey.withOpacity(.05),
-          borderRadius: BorderRadius.circular(15),
+              SizedBox(height: 10),
+              Text(
+                AppLocalizations.of(context)!.createYourOffer,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: LightColors.grey,
+                ),
+              ),
+            ],
+          ),
+          height: 171,
+          width: 171,
+          decoration: BoxDecoration(
+            color: LightColors.grey.withOpacity(.05),
+            borderRadius: BorderRadius.circular(15),
+          ),
         ),
       ),
     );
