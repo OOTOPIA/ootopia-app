ed091ad4657a93704adab5e02b4ef2a01effda11
Gracyele <gracyelesantossj@gmail.com>
2021-10-04 10:50:40 -0300

    feat: create product model

---
+++ lib/data/models/marketplace/product_model.dart
@@ -0,0 +1,5 @@
+class ProductModel{
+  final String title, description, imageUrl;
+  final double oozValue;
+  const ProductModel({required this.title, required this.description, required this.imageUrl, required this.oozValue});
+}
