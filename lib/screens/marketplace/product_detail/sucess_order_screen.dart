b4fb2af9661b0bd7513cf725669d711fed59fcde
Gracyele <gracyelesantossj@gmail.com>
2021-10-05 09:31:02 -0300

    feat: add responsive data values

--- lib/screens/marketplace/product_detail/sucess_order_screen.dart
+++ lib/screens/marketplace/product_detail/sucess_order_screen.dart
@@ -4,8 +4,6 @@ import 'package:ootopia_app/screens/marketplace/product_detail/components/get_ad
 import 'package:ootopia_app/theme/light/colors.dart';
 
 class SuccessOrderScreen extends StatelessWidget {
-  final TextStyle softStyle = TextStyle(color: Colors.white, fontSize: 18);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,36 +20,43 @@ class SuccessOrderScreen extends StatelessWidget {
             ),
             Container(
               height: MediaQuery.of(context).size.height,
-              margin: EdgeInsets.symmetric(horizontal: 26),
+              margin: EdgeInsets.symmetric(
+                  horizontal: getAdaptiveSize(26, context)),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Text(
                     "ETHICAL MARKETPLACE",
-                    style: TextStyle(color: Colors.white, fontSize: 22),
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: getAdaptiveSize(22, context)),
                   ),
                   Text(
                     "The place to exchange goods and services that positively impact our lives and the planet.",
-                    style: softStyle,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: getAdaptiveSize(18, context)),
                   ),
                   CircleAvatar(
                       backgroundColor: LightColors.blue,
                       radius: getAdaptiveSize(80, context),
                       child: Image.asset(
                         "assets/images/wallet.png",
-                        scale: 4,
+                        scale: MediaQuery.of(context).size.width > 720 ? 2 : 4,
                       )),
                   Text(
                     "THANKS FOR YOUR ORDER",
                     style: TextStyle(
                         color: Colors.white,
                         fontWeight: FontWeight.bold,
-                        fontSize: 28),
+                        fontSize: getAdaptiveSize(28, context)),
                   ),
                   Text(
                     "The vendor will soon contact you by email to arrange delivery of your purchase.",
-                    style: softStyle,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: getAdaptiveSize(18, context)),
                   ),
                   ElevatedButton(
                     style: ElevatedButton.styleFrom(
@@ -61,13 +66,14 @@ class SuccessOrderScreen extends StatelessWidget {
                         )),
                     onPressed: () {},
                     child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 18),
+                      padding: EdgeInsets.symmetric(
+                          horizontal: getAdaptiveSize(20, context),
+                          vertical: getAdaptiveSize(18, context)),
                       child: Text(
                         "Close",
                         style: TextStyle(
                             color: LightColors.silverText,
-                            fontSize: 16,
+                            fontSize: getAdaptiveSize(16, context),
                             fontWeight: FontWeight.bold),
                       ),
                     ),
