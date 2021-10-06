c68f83abea22076ec5161f5e8408f9e1ffb9e8d9
Gracyele <gracyelesantossj@gmail.com>
2021-10-05 22:54:08 -0300

    feat: position button as fixed at the end

--- lib/screens/marketplace/product_detail/product_detail_screen.dart
+++ lib/screens/marketplace/product_detail/product_detail_screen.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:ootopia_app/data/models/marketplace/product_model.dart';
+import 'package:ootopia_app/screens/marketplace/product_detail/components/get_adaptive_size.dart';
 import 'package:ootopia_app/screens/marketplace/product_detail/components/horizontal_expanded_image_widget.dart';
 import 'package:ootopia_app/screens/marketplace/product_detail/components/product_information_widget.dart';
 import 'package:ootopia_app/screens/marketplace/product_detail/components/profile_name_location_widget.dart';
 import 'package:ootopia_app/screens/marketplace/product_detail/components/purchase_button_widget.dart';
 import 'package:ootopia_app/screens/marketplace/product_detail/transfer_screen.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:smart_page_navigation/smart_page_navigation.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class ProductDetailScreen extends StatefulWidget {
   final SmartPageController controller;
@@ -21,34 +22,56 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.only(left: 26),
-                child: ProfileNameLocationWidget(
-                    profileImageUrl: widget.productModel.userPhotoUrl ?? "",
-                    profileName: widget.productModel.userName,
-                    location: widget.productModel.userLocation ?? ""),
+        child: LayoutBuilder(
+          builder: (context, constraint) {
+            return SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(minHeight: constraint.maxHeight),
+                child: IntrinsicHeight(
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(
+                          right: getAdaptiveSize(16, context),
+                          left: getAdaptiveSize(16, context),
+                        ),
+                        child: ProfileNameLocationWidget(
+                          profileImageUrl:
+                              widget.productModel.userPhotoUrl ?? "",
+                          profileName: widget.productModel.userName,
+                          location: widget.productModel.userLocation ?? "",
+                        ),
+                      ),
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            HorizontalExpandedImageWidget(
+                                urlImage: widget.productModel.photoUrl),
+                            ProductInformationWidget(
+                              productModel: widget.productModel,
+                              isContractible: false,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 26),
+                        child: PurchaseButtonWidget(
+                          title: AppLocalizations.of(context)!.purchaseNow,
+                          marginBottom: getAdaptiveSize(10, context),
+                          onPressed: () {
+                            widget.controller.insertPage(TransferScreen(
+                              productModel: widget.productModel,
+                            ));
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              HorizontalExpandedImageWidget(
-                  urlImage: widget.productModel.photoUrl),
-              ProductInformationWidget(
-                productModel: widget.productModel,
-              )
-            ],
-          ),
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: Container(
-        margin: EdgeInsets.symmetric(horizontal: 26),
-        child: PurchaseButtonWidget(
-          title: AppLocalizations.of(context)!.purchaseNow,
-          onPressed: () {
-            widget.controller.insertPage(TransferScreen(
-              productModel: widget.productModel,
-            ));
+            );
           },
         ),
       ),
