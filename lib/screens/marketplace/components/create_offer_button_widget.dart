aded9c745a1c4f3125ec96f03f544c442c4294ee
Caio Jesus <caio.nascimento@dcomp.ufs.br>
2021-10-07 09:06:38 -0300

    feat: make CreateOfferButton responsive

--- lib/screens/marketplace/components/create_offer_button_widget.dart
+++ lib/screens/marketplace/components/create_offer_button_widget.dart
@@ -13,42 +13,47 @@ class CreateOfferButtonWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final widthScreen = MediaQuery.of(context).size.width;
     return GestureDetector(
       onTap: () => onTap(),
-      child: Column(
-        children: [
-          SizedBox(height: 42),
-          DottedBorder(
-            strokeCap: StrokeCap.square,
-            dashPattern: [10, 10],
-            borderType: BorderType.RRect,
-            radius: Radius.circular(12),
-            child: Container(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SvgPicture.asset(
-                    'assets/icons/thin_add_icon.svg',
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          return Column(
+            children: [
+              SizedBox(height: 42),
+              DottedBorder(
+                strokeCap: StrokeCap.square,
+                dashPattern: [10, 10],
+                borderType: BorderType.RRect,
+                radius: Radius.circular(12),
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset(
+                        'assets/icons/thin_add_icon.svg',
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        AppLocalizations.of(context)!.createYourOffer,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: LightColors.grey,
+                        ),
+                      ),
+                    ],
                   ),
-                  SizedBox(height: 10),
-                  Text(
-                    AppLocalizations.of(context)!.createYourOffer,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: LightColors.grey,
-                    ),
+                  height: widthScreen >= 760 ? 171 : constraints.maxWidth / 2.2,
+                  width: widthScreen >= 760 ? 171 : constraints.maxWidth / 2.2,
+                  decoration: BoxDecoration(
+                    color: LightColors.grey.withOpacity(.05),
+                    borderRadius: BorderRadius.circular(15),
                   ),
-                ],
+                ),
               ),
-              height: 171,
-              width: 171,
-              decoration: BoxDecoration(
-                color: LightColors.grey.withOpacity(.05),
-                borderRadius: BorderRadius.circular(15),
-              ),
-            ),
-          ),
-        ],
+            ],
+          );
+        },
       ),
     );
   }
