fb68cd6c27838700df8df30b0130e1ef197b560e
anderson.barros <anderson.barros@vsaude.com.br>
2021-10-08 08:48:42 -0300

    fix: remove setstate watch video

--- lib/screens/learning_tracks/view_learning_tracks/watch_video_learning_tracks.dart
+++ lib/screens/learning_tracks/view_learning_tracks/watch_video_learning_tracks.dart
@@ -18,10 +18,7 @@ class WatchVideoLeaningTracks extends StatefulWidget {
 class _WatchVideoLeaningTracksState extends State<WatchVideoLeaningTracks> {
   @override
   Widget build(BuildContext context) {
-    var isPortrait = false;
-    setState(() {
-      isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
-    });
+    var isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
     return Scaffold(
       appBar: isPortrait
           ? AppBar(
