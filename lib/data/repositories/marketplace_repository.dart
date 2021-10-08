9984b2d8c44e15071801505ba65d3fa5972637a1
Gracyele <gracyelesantossj@gmail.com>
2021-10-08 09:43:58 -0300

    feat: change makePurchase type of return in repository

--- lib/data/repositories/marketplace_repository.dart
+++ lib/data/repositories/marketplace_repository.dart
@@ -1,10 +1,11 @@
 import 'dart:convert';
 
+import 'package:dio/dio.dart';
 import 'package:ootopia_app/data/repositories/api.dart';
 import 'package:ootopia_app/shared/secure-store-mixin.dart';
 
 abstract class IMarketplaceRepository {
-  Future<bool> makePurchase(
+  Future<Response<dynamic>> makePurchase(
       {required String productId, required String optionalMessage});
 }
 
@@ -12,16 +13,16 @@ class MarketplaceRepositoryImpl
     with SecureStoreMixin
     implements IMarketplaceRepository {
   @override
-  Future<bool> makePurchase(
+  Future<Response<dynamic>> makePurchase(
       {required String productId, required String optionalMessage}) async {
     final body = jsonEncode({"message": optionalMessage});
     try {
       final response = await ApiClient.api()
           .post("market-place" + "/$productId" + "/purchase", data: body);
 
-      return response.statusCode == 201;
+      return response;
     } catch (e) {
-      return throw Exception('failed to make purchase $e');
+      return throw e;
     }
   }
 }
