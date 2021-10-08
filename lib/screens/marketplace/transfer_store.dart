79b475e6633e4a0093e5fd13b04bc6d0c9a94fe8
Gracyele <gracyelesantossj@gmail.com>
2021-10-08 09:43:02 -0300

    feat: create transfer store

---
+++ lib/screens/marketplace/transfer_store.dart
@@ -0,0 +1,42 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:mobx/mobx.dart';
+import 'package:ootopia_app/data/repositories/marketplace_repository.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+part 'transfer_store.g.dart';
+
+class TransferStore = _TransferStoreBase with _$TransferStore;
+
+abstract class _TransferStoreBase with Store {
+  MarketplaceRepositoryImpl marketplaceRepositoryImpl =
+      MarketplaceRepositoryImpl();
+
+ 
+
+  @action
+  Future<bool> makePurchase(
+      {required String productId, required String optionalMessage}) async {
+    try {
+      final response = await marketplaceRepositoryImpl.makePurchase(
+          productId: productId, optionalMessage: optionalMessage);
+      return response.statusCode == 201;
+    } catch (e) {
+      if (e is DioError) throw Exception(e.response?.data["error"]);
+
+      throw Exception(e);
+    }
+  }
+
+  showSnackbarMessage(
+      {required BuildContext context, required String message}) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message.contains("INSUFFICIENT_BALANCE")
+            ? AppLocalizations.of(context)!.insufficientBalance
+            : AppLocalizations.of(context)!.generalError),
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+}
---
+++ lib/screens/marketplace/transfer_store.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'transfer_store.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
+
+mixin _$TransferStore on _TransferStoreBase, Store {
+  final _$makePurchaseAsyncAction =
+      AsyncAction('_TransferStoreBase.makePurchase');
+
+  @override
+  Future<bool> makePurchase(
+      {required String productId, required String optionalMessage}) {
+    return _$makePurchaseAsyncAction.run(() => super
+        .makePurchase(productId: productId, optionalMessage: optionalMessage));
+  }
+
+  @override
+  String toString() {
+    return '''
+
+    ''';
+  }
+}
