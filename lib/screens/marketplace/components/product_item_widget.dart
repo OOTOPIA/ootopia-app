6598cd3aad5f47640d0296be1969d6086cbc2b75
Caio Jesus <caio.nascimento@dcomp.ufs.br>
2021-10-15 09:22:13 -0300

    feat: make dynamic border using the image dimension

--- lib/screens/marketplace/components/product_item_widget.dart
+++ lib/screens/marketplace/components/product_item_widget.dart
@@ -2,131 +2,146 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:ootopia_app/data/models/marketplace/product_model.dart';
+import 'package:ootopia_app/shared/image_handler_mixin.dart';
 import 'package:ootopia_app/screens/marketplace/marketplace_store.dart';
 import 'package:ootopia_app/screens/marketplace/product_detail_screen.dart';
 import 'package:ootopia_app/theme/light/colors.dart';
 import 'package:provider/provider.dart';
 import 'package:smart_page_navigation/smart_page_navigation.dart';
 
-class ProductItem extends StatelessWidget {
+class ProductItem extends StatelessWidget with ImageHandler {
   final ProductModel productModel;
   ProductItem({Key? key, required this.productModel}) : super(key: key);
   final pageController = SmartPageController.getInstance();
 
+  Future<Size> getSize() async {
+    return getImageSize(productModel.imageUrl);
+  }
+
   @override
   Widget build(BuildContext context) {
     final marketplaceStore = Provider.of<MarketplaceStore>(context);
-    final widthScreen = MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.only(left: 12, right: 12, bottom: 24),
       child: InkWell(
         onTap: () => pageController
             .insertPage(ProductDetailScreen(productModel: productModel)),
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            return Container(
-              width: widthScreen >= 760
-                  ? (constraints.maxWidth / 4) - 24
-                  : (constraints.maxWidth / 2) - 24,
-              child: Column(
+        child: Container(
+          width: 171,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(50),
-                        ),
-                        child: ClipOval(
-                          child: Image(
-                            height: 36,
-                            fit: BoxFit.fitHeight,
-                            image: NetworkImage(productModel.userPhotoUrl),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(width: 8),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              productModel.userName,
-                              style: GoogleFonts.roboto(
-                                fontWeight: FontWeight.bold,
-                                fontSize: widthScreen <= 360 ? 12 : 14,
-                              ),
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 1,
-                            ),
-                            Text(
-                              '${productModel.location}',
-                              style: GoogleFonts.roboto(
-                                  fontSize: widthScreen <= 360 ? 10 : 12),
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 1,
-                              softWrap: false,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 7.6),
                   Container(
                     decoration: BoxDecoration(
-                      border: Border.all(
-                        width: 1,
-                        color: LightColors.grey,
-                      ),
-                      borderRadius: BorderRadius.circular(12),
+                      borderRadius: BorderRadius.circular(50),
                     ),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(12),
+                    child: ClipOval(
                       child: Image(
-                        height: 171,
-                        width: 171,
+                        height: 36,
                         fit: BoxFit.fitHeight,
-                        image: NetworkImage(productModel.imageUrl),
+                        image: NetworkImage(productModel.userPhotoUrl),
                       ),
                     ),
                   ),
-                  const SizedBox(height: 8),
-                  Text(
-                    productModel.title,
-                    style: GoogleFonts.roboto(fontWeight: FontWeight.bold),
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 2,
-                  ),
-                  const SizedBox(height: 7),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      SvgPicture.asset(
-                        'assets/icons/ooz_mini_blue.svg',
-                        height: 10.53,
-                        width: 20,
-                        color: LightColors.grey,
-                      ),
-                      SizedBox(width: 7),
-                      Text(
-                        marketplaceStore.currencyFormatter
-                            .format(productModel.price),
-                        style: GoogleFonts.roboto(
-                          color: LightColors.grey,
-                          fontWeight: FontWeight.w500,
-                          fontSize: 12,
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          productModel.userName,
+                          style: GoogleFonts.roboto(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
                         ),
-                      ),
-                    ],
+                        Text(
+                          '${productModel.location}',
+                          style: GoogleFonts.roboto(fontSize: 12),
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
+                          softWrap: false,
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
-            );
-          },
+              const SizedBox(height: 7.6),
+              FutureBuilder<Size>(
+                  future: getSize(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Container(
+                        height: 171,
+                        width: 171,
+                        decoration: BoxDecoration(
+                          color: LightColors.grey.withOpacity(.1),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      );
+                    }
+                    return Container(
+                      decoration: BoxDecoration(
+                        border: isWidthGreaterThanHeight(snapshot.data!)
+                            ? null
+                            : Border.all(
+                                width: 1,
+                                color: LightColors.grey,
+                              ),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(12),
+                        child: Image(
+                          height: 171,
+                          width: 171,
+                          fit: isWidthGreaterThanHeight(snapshot.data!)
+                              ? BoxFit.cover
+                              : BoxFit.fitHeight,
+                          image: NetworkImage(productModel.imageUrl),
+                        ),
+                      ),
+                    );
+                  }),
+              const SizedBox(height: 8),
+              Text(
+                productModel.title,
+                style: GoogleFonts.roboto(fontWeight: FontWeight.bold),
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              const SizedBox(height: 7),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SvgPicture.asset(
+                    'assets/icons/ooz_mini_blue.svg',
+                    height: 10.53,
+                    width: 20,
+                    color: LightColors.grey,
+                  ),
+                  SizedBox(width: 7),
+                  Text(
+                    marketplaceStore.currencyFormatter
+                        .format(productModel.price),
+                    style: GoogleFonts.roboto(
+                      color: LightColors.grey,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
