617dbe1138b445bf7b4cda1623ca934d93289baa
Gracyele <gracyelesantossj@gmail.com>
2021-10-15 18:23:39 -0300

    fix: centralizes content

--- lib/screens/marketplace/transfer_success_screen.dart
+++ lib/screens/marketplace/transfer_success_screen.dart
@@ -50,62 +50,77 @@ class _TransferSuccessScreenState extends State<TransferSuccessScreen> {
               fit: BoxFit.fill,
             ),
           ),
-          Container(
-            height: MediaQuery.of(context).size.height,
-            margin: EdgeInsets.symmetric(horizontal: 26),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Column(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.only(top: 50, bottom: 32),
-                      child: Text(
-                        AppLocalizations.of(context)!.ethicalMarketplace,
-                        style: GoogleFonts.roboto(
-                          color: Colors.white,
-                          fontSize: 22,
-                          shadows: [
-                            Shadow(
-                              offset: Offset(0, 5),
-                              blurRadius: 10,
-                              color: Colors.black.withOpacity(0.5),
-                            ),
-                          ],
+          Center(
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              margin: EdgeInsets.symmetric(horizontal: 26),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(top: 50, bottom: 32),
+                        child: Text(
+                          AppLocalizations.of(context)!.ethicalMarketplace,
+                          style: GoogleFonts.roboto(
+                            color: Colors.white,
+                            fontSize: 22,
+                            shadows: [
+                              Shadow(
+                                offset: Offset(0, 5),
+                                blurRadius: 10,
+                                color: Colors.black.withOpacity(0.5),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(right: 16),
-                      child: Text(
-                        AppLocalizations.of(context)!
-                            .ethicalMarketplaceDescription,
-                        style: GoogleFonts.roboto(
-                          color: Colors.white,
-                          fontSize: 18,
-                          shadows: [
-                            Shadow(
-                              offset: Offset(0, 5),
-                              blurRadius: 10,
-                              color: Colors.black.withOpacity(0.5),
-                            ),
-                          ],
+                      Padding(
+                        padding: const EdgeInsets.only(right: 16),
+                        child: Text(
+                          AppLocalizations.of(context)!
+                              .ethicalMarketplaceDescription,
+                          style: GoogleFonts.roboto(
+                            color: Colors.white,
+                            fontSize: 18,
+                            shadows: [
+                              Shadow(
+                                offset: Offset(0, 5),
+                                blurRadius: 10,
+                                color: Colors.black.withOpacity(0.5),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                Image.asset(
-                  "assets/images/wallet.png",
-                  scale: 2,
-                ),
-                Text(
-                  AppLocalizations.of(context)!.thanksForYourOrder,
-                  textAlign: TextAlign.center,
-                  style: GoogleFonts.roboto(
+                    ],
+                  ),
+                  Image.asset(
+                    "assets/images/wallet.png",
+                    scale: 2,
+                  ),
+                  Text(
+                    AppLocalizations.of(context)!.thanksForYourOrder,
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.roboto(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        shadows: [
+                          Shadow(
+                            offset: Offset(0, 5),
+                            blurRadius: 10,
+                            color: Colors.black.withOpacity(0.5),
+                          ),
+                        ],
+                        fontSize: 28),
+                  ),
+                  Text(
+                    AppLocalizations.of(context)!.messageVendorContact,
+                    style: GoogleFonts.roboto(
                       color: Colors.white,
-                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
                       shadows: [
                         Shadow(
                           offset: Offset(0, 5),
@@ -113,52 +128,39 @@ class _TransferSuccessScreenState extends State<TransferSuccessScreen> {
                           color: Colors.black.withOpacity(0.5),
                         ),
                       ],
-                      fontSize: 28),
-                ),
-                Text(
-                  AppLocalizations.of(context)!.messageVendorContact,
-                  style: GoogleFonts.roboto(
-                    color: Colors.white,
-                    fontSize: 18,
-                    shadows: [
-                      Shadow(
-                        offset: Offset(0, 5),
-                        blurRadius: 10,
-                        color: Colors.black.withOpacity(0.5),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(),
-                Container(
-                  margin: EdgeInsets.only(bottom: 24),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        primary: Colors.white,
-                        elevation: 0,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(45.0),
-                        )),
-                    onPressed: () {
-                      if (widget.goToMarketPlacePage)
-                        pageController.resetNavigation(
-                            redirectToBottomOptionIndex: 3);
-                      Navigator.pop(context);
-                    },
-                    child: Padding(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 20, vertical: 18),
-                      child: Text(
-                        AppLocalizations.of(context)!.close,
-                        style: GoogleFonts.roboto(
-                            color: LightColors.silverText,
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold),
-                      ),
                     ),
                   ),
-                )
-              ],
+                  SizedBox(),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 24),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.white,
+                          elevation: 0,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(45.0),
+                          )),
+                      onPressed: () {
+                        if (widget.goToMarketPlacePage)
+                          pageController.resetNavigation(
+                              redirectToBottomOptionIndex: 3);
+                        Navigator.pop(context);
+                      },
+                      child: Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 18),
+                        child: Text(
+                          AppLocalizations.of(context)!.close,
+                          style: GoogleFonts.roboto(
+                              color: LightColors.silverText,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
           Align(
