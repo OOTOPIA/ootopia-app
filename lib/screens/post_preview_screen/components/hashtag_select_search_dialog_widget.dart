91fe875a9118ca921532e3f5724fd034b042870e
Gracyele <gracyelesantossj@gmail.com>
2021-10-26 08:49:59 -0300

    feat: add close keyboard on tap the screen

--- lib/screens/post_preview_screen/components/hashtag_select_search_dialog_widget.dart
+++ lib/screens/post_preview_screen/components/hashtag_select_search_dialog_widget.dart
@@ -37,104 +37,107 @@ class _HashtagSelectSearchDialogWidgetState
   }
 
   Widget build(BuildContext context) {
-    return AlertDialog(
-      contentPadding: EdgeInsets.symmetric(horizontal: 16),
-      insetPadding: EdgeInsets.symmetric(horizontal: 10),
-      content: SingleChildScrollView(
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 24),
-                child: Text(
-                  AppLocalizations.of(context)!.selectAtLeast1Tag,
-                  style: GoogleFonts.roboto(
-                      fontSize: 22, fontWeight: FontWeight.w600),
-                  textAlign: TextAlign.start,
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).requestFocus(new FocusNode()),
+      child: AlertDialog(
+        contentPadding: EdgeInsets.symmetric(horizontal: 16),
+        insetPadding: EdgeInsets.symmetric(horizontal: 10),
+        content: SingleChildScrollView(
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 24),
+                  child: Text(
+                    AppLocalizations.of(context)!.selectAtLeast1Tag,
+                    style: GoogleFonts.roboto(
+                        fontSize: 22, fontWeight: FontWeight.w600),
+                    textAlign: TextAlign.start,
+                  ),
                 ),
-              ),
-              TextFormField(
-                onChanged: (value) {
-                  widget.postPreviewScreenStore.onFilterTagChanged(value);
-                },
-                decoration: InputDecoration(
-                  hintText: AppLocalizations.of(context)!.searchForAtag,
-                  hintStyle: TextStyle(color: Colors.black.withOpacity(0.2)),
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.zero,
-                      borderSide:
-                          BorderSide(color: Color(0xff707070), width: 0.25)),
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.zero,
-                      borderSide:
-                          BorderSide(color: Color(0xff707070), width: 0.25)),
+                TextFormField(
+                  onChanged: (value) {
+                    widget.postPreviewScreenStore.onFilterTagChanged(value);
+                  },
+                  decoration: InputDecoration(
+                    hintText: AppLocalizations.of(context)!.searchForAtag,
+                    hintStyle: TextStyle(color: Colors.black.withOpacity(0.2)),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.zero,
+                        borderSide:
+                            BorderSide(color: Color(0xff707070), width: 0.25)),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.zero,
+                        borderSide:
+                            BorderSide(color: Color(0xff707070), width: 0.25)),
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 30),
-                child: Divider(
-                  thickness: 1,
-                  height: 1,
+                Padding(
+                  padding: const EdgeInsets.only(top: 30),
+                  child: Divider(
+                    thickness: 1,
+                    height: 1,
+                  ),
                 ),
-              ),
-              Observer(
-                builder: (context) => Container(
-                  height: MediaQuery.of(context).size.height * 0.6,
-                  padding: EdgeInsets.symmetric(vertical: 10),
-                  child: SingleChildScrollView(
-                    child: Wrap(
-                      children:
-                          widget.postPreviewScreenStore.filterValue.isEmpty
-                              ? allTags
-                              : widget.postPreviewScreenStore
-                                  .filterTagsPerName(allTags),
+                Observer(
+                  builder: (context) => Container(
+                    height: MediaQuery.of(context).size.height * 0.6,
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    child: SingleChildScrollView(
+                      child: Wrap(
+                        children:
+                            widget.postPreviewScreenStore.filterValue.isEmpty
+                                ? allTags
+                                : widget.postPreviewScreenStore
+                                    .filterTagsPerName(allTags),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Divider(
-                thickness: 1,
-                height: 1,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 6),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    TextButton(
-                      onPressed: () {
-                        widget.postPreviewScreenStore.selectedTags.clear();
-                        widget.postPreviewScreenStore.filterValue = "";
-                        Navigator.pop(context);
-                      },
-                      child: Text(
-                        AppLocalizations.of(context)!.cancel,
-                        style: GoogleFonts.roboto(
-                            color: Color(0xff018F9C),
-                            fontWeight: FontWeight.w600,
-                            fontSize: 16),
-                      ),
-                    ),
-                    TextButton(
+                Divider(
+                  thickness: 1,
+                  height: 1,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 6),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
                         onPressed: () {
+                          widget.postPreviewScreenStore.selectedTags.clear();
                           widget.postPreviewScreenStore.filterValue = "";
-
                           Navigator.pop(context);
                         },
                         child: Text(
-                          AppLocalizations.of(context)!.confirm,
+                          AppLocalizations.of(context)!.cancel,
                           style: GoogleFonts.roboto(
                               color: Color(0xff018F9C),
                               fontWeight: FontWeight.w600,
                               fontSize: 16),
-                        ))
-                  ],
-                ),
-              )
-            ],
+                        ),
+                      ),
+                      TextButton(
+                          onPressed: () {
+                            widget.postPreviewScreenStore.filterValue = "";
+
+                            Navigator.pop(context);
+                          },
+                          child: Text(
+                            AppLocalizations.of(context)!.confirm,
+                            style: GoogleFonts.roboto(
+                                color: Color(0xff018F9C),
+                                fontWeight: FontWeight.w600,
+                                fontSize: 16),
+                          ))
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
