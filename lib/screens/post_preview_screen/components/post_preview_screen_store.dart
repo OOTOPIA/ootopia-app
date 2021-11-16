ffdf5ca6174299d5c711ba6e682b4397556ef72b
anderson.barros <anderson.barros@vsaude.com.br>
2021-11-16 17:02:04 -0300

    feat: ajust in snackbar

--- lib/screens/post_preview_screen/components/post_preview_screen_store.dart
+++ lib/screens/post_preview_screen/components/post_preview_screen_store.dart
@@ -86,8 +86,9 @@ abstract class _PostPreviewScreenStoreBase with Store {
     List<HashtagWidget> filteredtagsList = [];
 
     filteredtagsList = allTags
-        .where(
-            (hashtagWidget) => hashtagWidget.item.label.toLowerCase().contains(filterValue.toLowerCase()))
+        .where((hashtagWidget) => hashtagWidget.item.label
+            .toLowerCase()
+            .contains(filterValue.toLowerCase()))
         .toList();
     filteredtagsList.map((item) => item).toList();
 
@@ -111,7 +112,7 @@ abstract class _PostPreviewScreenStoreBase with Store {
       uploadIsLoading = true;
       var result = await this._createPost(post);
       double oozToRewardForVideo = 25;
-      double oozToRewardForImage = 15;
+      double oozToRewardForImage = 5;
       if (post.durationInSecs != null && post.type == "video") {
         oozToReward = oozToRewardForVideo * (post.durationInSecs! / 60);
       } else if (post.type == "image") {
