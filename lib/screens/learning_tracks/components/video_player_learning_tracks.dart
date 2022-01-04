91e237a2b57c43d42a66e1917685505d9651ceb6
Leandro Pereira <leandro-pereira2011@hotmail.com>
2022-01-04 14:17:46 -0300

    fix(*): fix video player in IOS

--- lib/screens/learning_tracks/components/video_player_learning_tracks.dart
+++ lib/screens/learning_tracks/components/video_player_learning_tracks.dart
@@ -6,6 +6,7 @@ import 'package:flutter/services.dart';
 import 'package:loading_overlay/loading_overlay.dart';
 import 'package:ootopia_app/screens/learning_tracks/components/video_bar.dart';
 import 'package:ootopia_app/screens/learning_tracks/components/video_play_and_pause.dart';
+import 'package:wakelock/wakelock.dart';
 import 'package:video_player/video_player.dart';
 import 'package:visibility_detector/visibility_detector.dart';
 
@@ -29,15 +30,36 @@ class VideoPlayerLearningTracks extends StatefulWidget {
 
 class _VideoPlayerLearningTracksState extends State<VideoPlayerLearningTracks> {
   late VideoPlayerController videoPlayerController;
+
+  String totalTimeVideoText = '';
+  String positionVideoText = '';
+  int currentPosition = 0;
+  double maxDurationVideo = 0;
+  bool isWakelock = false;
   Timer? timerOpacity;
   var widthVideo = 1.0;
   var heightVideo = 1.0;
   bool isLoading = false;
   bool fullScreenVideo = false;
 
+  String timeVideo(Duration time) {
+    String twoDigits(int n) => n.toString().padLeft(2, "0");
+    String twoDigitHours =
+        time.inHours == 0 ? '' : twoDigits(time.inHours.remainder(60)) + ':';
+    String twoDigitMinutes = twoDigits(time.inMinutes.remainder(60));
+    String twoDigitSeconds = twoDigits(time.inSeconds.remainder(60));
+    return "$twoDigitHours$twoDigitMinutes:$twoDigitSeconds";
+  }
+
   @override
   void initState() {
     super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+    ]);
     isLoading = true;
 
     videoPlayerController = VideoPlayerController.network(widget.videoUrl)
@@ -46,7 +68,22 @@ class _VideoPlayerLearningTracksState extends State<VideoPlayerLearningTracks> {
           setState(() {
             widthVideo = videoPlayerController.value.size.width;
             heightVideo = videoPlayerController.value.size.height;
+            maxDurationVideo =
+                videoPlayerController.value.duration.inSeconds.toDouble();
+            totalTimeVideoText =
+                timeVideo(videoPlayerController.value.duration);
+            positionVideoText = timeVideo(videoPlayerController.value.position);
+            currentPosition = videoPlayerController.value.position.inSeconds;
           });
+          if (!isWakelock && videoPlayerController.value.isPlaying) {
+            Wakelock.enable();
+            isWakelock = true;
+          } else {
+            if (!videoPlayerController.value.isPlaying) {
+              Wakelock.disable();
+              isWakelock = false;
+            }
+          }
         }
       })
       ..initialize().then((value) {
@@ -89,121 +126,240 @@ class _VideoPlayerLearningTracksState extends State<VideoPlayerLearningTracks> {
 
   @override
   Widget build(BuildContext context) {
-    return OrientationBuilder(
-      builder: (context, orientation) {
-        if (!fullScreenVideo) {
-          SystemChrome.setPreferredOrientations([
-            DeviceOrientation.portraitUp,
-          ]);
-          if (MediaQuery.of(context).orientation == Orientation.portrait) {
-            if (widthVideo > heightVideo) {
-              heightPlayerVideo = MediaQuery.of(context).size.width /
-                  (widthVideo / heightVideo);
-            } else {
-              heightPlayerVideo = MediaQuery.of(context).size.height * 0.75;
-            }
+    return OrientationBuilder(builder: (context, orientation) {
+      if (!fullScreenVideo) {
+        if (MediaQuery.of(context).orientation == Orientation.portrait) {
+          if (widthVideo > heightVideo) {
+            heightPlayerVideo =
+                MediaQuery.of(context).size.width / (widthVideo / heightVideo);
           } else {
-            heightPlayerVideo = MediaQuery.of(context).size.height;
+            heightPlayerVideo = MediaQuery.of(context).size.height * 0.75;
           }
         } else {
           heightPlayerVideo = MediaQuery.of(context).size.height;
-          SystemChrome.setPreferredOrientations([
-            DeviceOrientation.portraitUp,
-            DeviceOrientation.portraitDown,
-            DeviceOrientation.landscapeRight,
-            DeviceOrientation.landscapeLeft,
-          ]);
         }
-        return LoadingOverlay(
-          isLoading: isLoading,
-          child: VisibilityDetector(
-            key: Key('${videoPlayerController.dataSource}'),
-            onVisibilityChanged: (visibility) {
-              if (visibility.visibleFraction == 0 && this.mounted) {
-                videoPlayerController.pause();
-              }
-            },
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  InkWell(
-                    onTap: () {
-                      setState(() {
-                        timerOpacity?.cancel();
-                        timerOpacity = Timer(
-                          Duration(seconds: 1),
-                          () => setState(() => timerOpacity = null),
-                        );
-                      });
-                    },
+      } else {
+        heightPlayerVideo = MediaQuery.of(context).size.height;
+      }
+      return LoadingOverlay(
+        isLoading: isLoading,
+        child: VisibilityDetector(
+          key: Key('${videoPlayerController.dataSource}'),
+          onVisibilityChanged: (visibility) {
+            if (visibility.visibleFraction == 0 && this.mounted) {
+              videoPlayerController.pause();
+            }
+          },
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      timerOpacity?.cancel();
+                      timerOpacity = Timer(
+                        Duration(seconds: 1),
+                        () => setState(() => timerOpacity = null),
+                      );
+                    });
+                  },
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: heightPlayerVideo,
                     child: Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: heightPlayerVideo,
-                      child: Container(
-                        child: Stack(
-                          children: [
-                            Image.network(
-                              widget.thumbVideo,
-                              fit: BoxFit.cover,
-                              width: double.infinity,
-                            ),
-                            ClipRect(
-                              child: BackdropFilter(
-                                filter: ImageFilter.blur(
-                                  sigmaX: 40,
-                                  sigmaY: 40,
-                                ),
-                                child: Container(
-                                  color: Colors.black.withOpacity(0.4),
-                                ),
+                      child: Stack(
+                        children: [
+                          Image.network(
+                            widget.thumbVideo,
+                            fit: BoxFit.cover,
+                            width: double.infinity,
+                          ),
+                          ClipRect(
+                            child: BackdropFilter(
+                              filter: ImageFilter.blur(
+                                sigmaX: 40,
+                                sigmaY: 40,
+                              ),
+                              child: Container(
+                                color: Colors.black.withOpacity(0.4),
                               ),
                             ),
-                            Container(
-                              alignment: Alignment.center,
-                              height: heightPlayerVideo,
-                              child: AspectRatio(
-                                aspectRatio: widthVideo / heightVideo,
-                                child: VideoPlayer(videoPlayerController),
-                              ),
+                          ),
+                          Container(
+                            alignment: Alignment.center,
+                            height: heightPlayerVideo,
+                            child: AspectRatio(
+                              aspectRatio: widthVideo / heightVideo,
+                              child: VideoPlayer(videoPlayerController),
                             ),
-                            Align(
-                              alignment: Alignment.center,
-                              child: VideoPlayAndPause(
-                                onClickSlider: onClickSlider,
-                                videoPlayerController: videoPlayerController,
-                                timerOpacity: timerOpacity,
-                              ),
-                            ),
-                            Align(
-                              alignment: Alignment.bottomCenter,
-                              child: VideoBar(
-                                videoPlayerController: videoPlayerController,
-                                fullScreenVideo: fullScreenVideo,
-                                fullScreenEvent: () {
-                                  fullScreenVideo = !fullScreenVideo;
-                                  widget.eventFullScreen();
-                                },
-                                loadingTimeLineVideo: (bool value) {
+                          ),
+                          Align(
+                            alignment: Alignment.center,
+                            child: onClickSlider
+                                ? Center(
+                                    child: CircularProgressIndicator(
+                                      color: Color(0xff35AD6C),
+                                    ),
+                                  )
+                                : AnimatedOpacity(
+                                    opacity: timerOpacity != null ? 1 : 0.0,
+                                    duration: Duration(milliseconds: 200),
+                                    child: GestureDetector(
+                                      onTap: () {
+                                        setState(() {
+                                          videoPlayerController.value.isPlaying
+                                              ? videoPlayerController.pause()
+                                              : videoPlayerController.play();
+                                        });
+                                      },
+                                      child: CircleAvatar(
+                                        backgroundColor: Color(0xff35AD6C),
+                                        radius: 28.5,
+                                        child: Icon(
+                                          (videoPlayerController.value.isPlaying
+                                              ? Icons.pause
+                                              : Icons.play_arrow),
+                                          size: 23,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                          ),
+                          Align(
+                            alignment: Alignment.bottomCenter,
+                            child: AnimatedOpacity(
+                              opacity: timerOpacity != null ? 1 : 0.0,
+                              duration: Duration(milliseconds: 200),
+                              child: GestureDetector(
+                                onTap: () {
                                   setState(() {
-                                    onClickSlider = value;
+                                    timerOpacity?.cancel();
+                                    timerOpacity = Timer(
+                                      Duration(seconds: 1),
+                                      () => setState(() => timerOpacity = null),
+                                    );
                                   });
                                 },
-                                timerOpacity: timerOpacity,
+                                child: Container(
+                                  height: 40,
+                                  width: double.infinity,
+                                  padding: EdgeInsets.symmetric(horizontal: 24),
+                                  child: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Text(
+                                        '$positionVideoText',
+                                        style: TextStyle(
+                                          color: Color(0xffCDCDCD),
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        width: 10,
+                                      ),
+                                      Expanded(
+                                        child: SliderTheme(
+                                          data: SliderThemeData(
+                                            trackHeight: 2,
+                                            thumbShape: RoundSliderThumbShape(
+                                                enabledThumbRadius: 6.5),
+                                            trackShape: CustomTrackShape(),
+                                          ),
+                                          child: Slider(
+                                            inactiveColor: Color(0xffCDCDCD),
+                                            activeColor: Color(0xff35ad6c),
+                                            thumbColor: Color(0xff35ad6c),
+                                            min: 0,
+                                            max: maxDurationVideo,
+                                            value: currentPosition.toDouble(),
+                                            onChangeStart: (value) async {
+                                              await videoPlayerController
+                                                  .pause();
+                                              setState(() {
+                                                onClickSlider = true;
+                                                Future.delayed(
+                                                    Duration(milliseconds: 300),
+                                                    () {
+                                                  onClickSlider = false;
+                                                });
+                                              });
+                                            },
+                                            onChanged: (value) async {
+                                              setState(() {
+                                                totalTimeVideoText = timeVideo(
+                                                    videoPlayerController
+                                                        .value.duration);
+                                                positionVideoText = timeVideo(
+                                                    videoPlayerController
+                                                        .value.position);
+                                                currentPosition = value.toInt();
+                                                onClickSlider = false;
+                                              });
+                                              await videoPlayerController
+                                                  .seekTo(Duration(
+                                                      seconds: value.toInt()));
+                                            },
+                                            onChangeEnd: (value) async {
+                                              setState(() {
+                                                onClickSlider = false;
+                                              });
+                                              await videoPlayerController
+                                                  .play();
+                                            },
+                                          ),
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        width: 10,
+                                      ),
+                                      Text(
+                                        '$totalTimeVideoText',
+                                        style: TextStyle(
+                                          color: Color(0xffCDCDCD),
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        width: 10,
+                                      ),
+                                      IconButton(
+                                        onPressed: () {
+                                          fullScreenVideo = !fullScreenVideo;
+                                          widget.eventFullScreen();
+                                        },
+                                        icon: fullScreenVideo
+                                            ? ImageIcon(
+                                                AssetImage(
+                                                    "assets/icons/icon_minimizescreen.png"),
+                                                color: Color(0xFFCDCDCD),
+                                                size: 16)
+                                            : ImageIcon(
+                                                AssetImage(
+                                                    "assets/icons/icon_maximizescreen.png"),
+                                                color: Color(0xFFCDCDCD),
+                                                size: 16),
+                                      ),
+                                    ],
+                                  ),
+                                ),
                               ),
-                            )
-                          ],
-                        ),
+                            ),
+                          )
+                        ],
                       ),
                     ),
                   ),
-                  if (!fullScreenVideo) widget.viewQuiz,
-                ],
-              ),
+                ),
+                if (!fullScreenVideo) widget.viewQuiz,
+              ],
             ),
           ),
-        );
-      },
-    );
+        ),
+      );
+    });
   }
 }
 
