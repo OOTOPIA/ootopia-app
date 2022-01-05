b5c22aac2f33e1d8adf4f683861f541fb23bb616
arielsam67@gmail.com <you@example.com>
2022-01-05 14:18:30 -0300

    add ripple animation

--- lib/screens/persona_level/personal_level.dart
+++ lib/screens/persona_level/personal_level.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:ootopia_app/screens/auth/auth_store.dart';
+import 'package:ootopia_app/screens/persona_level/ripple_animation.dart';
+import 'package:ootopia_app/screens/persona_level/ripple_animationA.dart';
+import 'package:provider/provider.dart';
 
 class PersonaLevel extends StatefulWidget {
-
 
   @override
   _PersonaLevelState createState() => _PersonaLevelState();
 }
 
 class _PersonaLevelState extends State<PersonaLevel> {
+  late AuthStore? authStore;
+  late String urlImage;
+
+
+
+  _init(BuildContext context){
+    authStore = Provider.of<AuthStore>(context);
+    urlImage = authStore?.currentUser?.photoUrl ?? '';
+  }
+
 
   @override
   Widget build(BuildContext context) {
+    _init(context);
     return Container(
       height: MediaQuery.of(context).size.height - (180 + 59 + 6),
       width: MediaQuery.of(context).size.height,
@@ -20,6 +34,8 @@ class _PersonaLevelState extends State<PersonaLevel> {
             image: AssetImage(
               'assets/images/map.png',
             ),
+            //alignment: Alignment.centerLeft,
+            alignment: Alignment(-0.5, -1.0),
             fit: BoxFit.cover,
           )),
       child: Stack(
@@ -30,7 +46,25 @@ class _PersonaLevelState extends State<PersonaLevel> {
               margin: EdgeInsets.only(bottom: MediaQuery.of(context).size.width/3),
               height: MediaQuery.of(context).size.width/3,
               width: MediaQuery.of(context).size.width/3,
-              color: Colors.brown,
+              color: Colors.transparent,
+              child: RipplesAnimation(
+                color: Color(0xffB1E3FD),
+                size: MediaQuery.of(context).size.width/3,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(100),
+                  child: urlImage.isEmpty
+                      ? Image.asset(
+                    'assets/icons/user.png',
+                    height: MediaQuery.of(context).size.width/3 - MediaQuery
+                        .of(context).size.width/9,
+                    fit: BoxFit.cover,
+                  )
+                      : Image.network(
+                    urlImage,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
             ),
           )
         ],
