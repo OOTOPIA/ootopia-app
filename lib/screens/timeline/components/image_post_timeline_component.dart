24151071395e8ac4405eee54c6d614dc6d392857
arielsam67@gmail.com <you@example.com>
2022-01-07 10:55:03 -0300

    set boxFit to imagem and add blur

--- lib/screens/timeline/components/image_post_timeline_component.dart
+++ lib/screens/timeline/components/image_post_timeline_component.dart
@@ -1,5 +1,6 @@
 import 'dart:ui' as ui;
 import 'dart:async';
+import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class ImagePostTimeline extends StatefulWidget {
@@ -21,18 +22,25 @@ class _ImagePostTimeline extends State<ImagePostTimeline> {
   Widget build(BuildContext context) {
     Image image = Image.network(
       widget.image,
-      fit: BoxFit.cover,
     );
-    Size imageSize = Size(100.toDouble(), 100.toDouble());
+    BoxFit boxFit = BoxFit.cover;
+    Size imageSize = Size(100.0, 100.0);
 
     Completer<ui.Image> completer = Completer<ui.Image>();
     image.image.resolve(ImageConfiguration()).addListener(ImageStreamListener(
       (ImageInfo image, bool synchronousCall) {
-        imageSize =
-            Size(image.image.width.toDouble(), image.image.height.toDouble());
+        imageSize = Size(image.image.width.toDouble(), image.image.height.toDouble());
+        if(imageSize.height <= imageSize.width){
+          boxFit = BoxFit.fitWidth;
+        }else{
+          boxFit = BoxFit.fitHeight;
+        }
+        if (mounted) setState(() {});
         completer.complete(image.image);
       },
     ));
+
+
 
     return GestureDetector(
       onDoubleTap: () {
@@ -44,7 +52,7 @@ class _ImagePostTimeline extends State<ImagePostTimeline> {
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.width,
         decoration: BoxDecoration(
-            color: Color(0xff000000),
+            color: Color(0xff000000).withOpacity(1),
             borderRadius: BorderRadius.only(
                 bottomLeft: Radius.circular(20),
                 bottomRight: Radius.circular(20)),
@@ -52,7 +60,42 @@ class _ImagePostTimeline extends State<ImagePostTimeline> {
               fit: BoxFit.cover,
               alignment: FractionalOffset.center,
               image: NetworkImage(widget.image),
-            )),
+            )
+        ),
+        child: Stack(
+          children: [
+            //BLUR
+            ClipRRect(
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20)),
+              child: BackdropFilter(
+                  filter: new ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.width,
+                  )),
+            ),
+
+            //IMAGE
+            Align(
+              alignment: Alignment.center,
+              child:  ClipRRect(
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20)),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.width,
+                  child: Image.network(
+                      widget.image,
+                      fit: boxFit,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
