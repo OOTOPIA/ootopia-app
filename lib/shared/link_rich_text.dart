7fef7cc1051a76947f3bc11bac01cb2436a99382
PauloVFonseca <paulo.oliveira@devmagic.com.br>
2022-01-10 09:58:33 -0300

    feat: create rich text for link

---
+++ lib/shared/link_rich_text.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:ootopia_app/theme/light/colors.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class LinkRichText extends StatelessWidget {
+  final String? text;
+  final int? maxLines;
+  final Key? key;
+  final RegExp regExp = RegExp(
+      r"((https?:www\.)|(https?:\/\/)|(www\.))?[\w/\-?=%.][-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9]{1,6}(\/[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)?");
+  final List<InlineSpan> textSpanWidget = <TextSpan>[];
+
+  LinkRichText(
+    this.text, {
+    this.maxLines,
+    this.key,
+  }) {
+    _initialize();
+  }
+
+  _initialize() {
+    if (regExp.hasMatch(text!))
+      _hasLink();
+    else
+      _hasntLink();
+  }
+
+  _hasntLink() {
+    textSpanWidget.add(TextSpan(
+      text: this.text,
+      style: TextStyle(
+        color: LightColors.black,
+        fontWeight: FontWeight.w400,
+        fontSize: 14,
+      ),
+    ));
+  }
+
+  _hasLink() {
+    final splitedText = text?.split(' ');
+
+    splitedText?.forEach((element) {
+      if (regExp.hasMatch(element)) {
+        var url = element.toString().contains('https://')
+            ? element
+            : element.toString().contains('http://')
+                ? element
+                : 'http://' + element;
+
+        textSpanWidget.add(TextSpan(
+            text: element,
+            style: TextStyle(
+              color: LightColors.linkText,
+              fontWeight: FontWeight.w400,
+              fontSize: 14,
+            ),
+            recognizer: new TapGestureRecognizer()..onTap = () => launch(url)));
+      } else
+        textSpanWidget.add(TextSpan(
+          text: element,
+          style: TextStyle(
+            color: LightColors.black,
+            fontWeight: FontWeight.w400,
+            fontSize: 14,
+          ),
+        ));
+      textSpanWidget.add(TextSpan(text: " "));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: RichText(
+        textAlign: TextAlign.start,
+        overflow: TextOverflow.clip,
+        maxLines: maxLines,
+        text: TextSpan(children: textSpanWidget),
+      ),
+    );
+  }
+}
