fe3cd7b1ea77bca3f671b95fe1f79a75704bb6ed
PauloVFonseca <paulo.oliveira@devmagic.com.br>
2022-01-17 13:51:01 -0300

    feat: finish notification on background

--- lib/shared/FirebaseMessaging/push_notification_background.dart
+++ lib/shared/FirebaseMessaging/push_notification_background.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:io';
 import 'dart:typed_data';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
@@ -10,21 +9,21 @@ import 'package:ootopia_app/data/models/users/user_model.dart';
 import 'package:ootopia_app/data/repositories/post_repository.dart';
 import 'package:ootopia_app/data/repositories/user_repository.dart';
 import 'package:ootopia_app/screens/profile_screen/components/timeline_profile.dart';
+import 'package:ootopia_app/shared/app_usage_splash_screen.dart';
 import 'package:ootopia_app/shared/secure-store-mixin.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:http/http.dart' as http;
 import 'package:smart_page_navigation/smart_page_navigation.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class PushNotificationBackground {
   FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
-  SecureStoreMixin storage = SecureStoreMixin();
   UserRepositoryImpl userRepository = UserRepositoryImpl();
   PostRepositoryImpl postsRepository = PostRepositoryImpl();
+  AppUsageSplashScreen appUsageSplashScreen = AppUsageSplashScreen();
   SmartPageController? controller;
   late AndroidNotificationChannel channel;
   FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
       FlutterLocalNotificationsPlugin();
-  User? user;
   String? payload;
 
   String? token;
@@ -36,11 +35,6 @@ class PushNotificationBackground {
       description: 'This channel is used for important notifications.',
       importance: Importance.high,
     );
-
-    Future.delayed(Duration.zero, () async {
-      bool loggedIn = await storage.getUserIsLoggedIn();
-      if (loggedIn) user = await storage.getCurrentUser();
-    });
 
     Future.delayed(Duration.zero, () async {
       await flutterLocalNotificationsPlugin
@@ -72,36 +66,36 @@ class PushNotificationBackground {
   }
 
   showNotificationOnBackground(RemoteMessage message) async {
-      print("Message ${message.data}");
-      message.data["usersName"] = jsonDecode(message.data["usersName"]);
-      final notification = NotificationModel.fromJson(message.data);
+    print("Message ${message.data}");
+    message.data["usersName"] = jsonDecode(message.data["usersName"]);
+    final notification = NotificationModel.fromJson(message.data);
 
-      String title = getNotificationTitle(notification.type);
-      String body =
-          getNotificationBody(notification.type, notification.usersName);
+    String title = await getNotificationTitle(notification.type);
+    String body =
+        await getNotificationBody(notification.type, notification.usersName);
 
-      ByteArrayAndroidBitmap? bigIcon =
-          await _turnPhotoURLIntoBitmap(notification.photoURL!);
+    ByteArrayAndroidBitmap? bigIcon =
+        await _turnPhotoURLIntoBitmap(notification.photoURL!);
 
-      if (message.data != {} || message.data != null) {
-        flutterLocalNotificationsPlugin.show(
-          notification.hashCode,
-          "title",
-          "body",
-          NotificationDetails(
-            android: AndroidNotificationDetails(
-              channel.id,
-              channel.name,
-              color: Colors.white,
-              channelDescription: channel.description,
-              icon: '@mipmap/ic_launcher',
-              largeIcon: bigIcon!,
-              styleInformation: BigTextStyleInformation("body"),
-            ),
+    if (message.data != {} || message.data != null) {
+      flutterLocalNotificationsPlugin.show(
+        notification.hashCode,
+        title,
+        body,
+        NotificationDetails(
+          android: AndroidNotificationDetails(
+            channel.id,
+            channel.name,
+            color: Colors.white,
+            channelDescription: channel.description,
+            icon: '@mipmap/ic_launcher',
+            largeIcon: bigIcon!,
+            styleInformation: BigTextStyleInformation(body),
           ),
-         payload: notification.postId,
-        );
-      }
+        ),
+        payload: notification.postId,
+      );
+    }
   }
 
   _turnPhotoURLIntoBitmap(String photoURL) async {
@@ -115,46 +109,61 @@ class PushNotificationBackground {
     return response.bodyBytes;
   }
 
-  String getNotificationTitle(String type, {int? oozReceived}) {
-     return "";
-    // if (type == "gratitude_reward")
-    //   return AppLocalizations.of(context!)!
-    //       .notificationTitleOOzReceived
-    //       .replaceAll('%OOZ_RECEIVED%', '${oozReceived.toString()}');
-    // else
-    //   return AppLocalizations.of(context!)!
-    //       .notificationTitleCommentedPost
-    //       .replaceAll('%YOUR_NAME%', '${user!.fullname!.split(" ").first}');
+  languageLocale() async {
+    String language = await appUsageSplashScreen.checkLanguageConfig();
+    return Locale(language);
   }
 
-  String getNotificationBody(String type, List<String> usersName) {
-    return "";
-    // if (type == "gratitude_reward") {
-    //   if (usersName.length == 1)
-    //     return AppLocalizations.of(context!)!
-    //         .notificationBodyOOzReceivedByOnePerson
-    //         .replaceAll('%USER_NAME%', '${usersName.first}');
-    //   else
-    //     return AppLocalizations.of(context!)!
-    //         .notificationBodyOOzReceivedBySomePeople
-    //         .replaceAll('%USER_NAME%', '${usersName.last}')
-    //         .replaceAll(
-    //             '%PEOPLE_AMOUNT%', '${(usersName.length - 1).toString()}');
-    // } else {
-    //   if (usersName.length == 1)
-    //     return AppLocalizations.of(context!)!
-    //         .notificationBodyCommentedPostByOnePerson
-    //         .replaceAll('%USER_NAME%', '${usersName.first}');
-    //   else
-    //     return AppLocalizations.of(context!)!
-    //         .notificationBodyCommentedPostBySomePeople
-    //         .replaceAll('%USER_NAME%', '${usersName.last}')
-    //         .replaceAll(
-    //             '%PEOPLE_AMOUNT%', '${(usersName.length - 1).toString()}');
-    // }
+  Future<String> getNotificationTitle(String type, {int? oozReceived}) async {
+    Locale locale = await languageLocale();
+    User? user = await getUserData();
+
+    String titleText = "";
+
+    AppLocalizations.delegate.load(locale).then((value) {
+      if (type == "gratitude_reward") {
+        titleText = value.notificationTitleOOzReceived
+            .replaceAll('%OOZ_RECEIVED%', '${oozReceived.toString()}');
+      } else {
+        titleText = value.notificationTitleCommentedPost
+            .replaceAll('%YOUR_NAME%', '${user!.fullname!.split(" ").first}');
+      }
+    });
+    return titleText;
   }
 
-  goToTimelinePost(List<TimelinePost> posts) {
+  Future<String> getNotificationBody(
+      String type, List<String> usersName) async {
+    Locale locale = await languageLocale();
+
+    String bodyText = "";
+
+    AppLocalizations.delegate.load(locale).then((value) {
+      if (type == "gratitude_reward") {
+        if (usersName.length == 1)
+          bodyText = value.notificationBodyOOzReceivedByOnePerson
+              .replaceAll('%USER_NAME%', '${usersName.first}');
+        else
+          bodyText = value.notificationBodyOOzReceivedBySomePeople
+              .replaceAll('%USER_NAME%', '${usersName.last}')
+              .replaceAll(
+                  '%PEOPLE_AMOUNT%', '${(usersName.length - 1).toString()}');
+      } else {
+        if (usersName.length == 1)
+          bodyText = value.notificationBodyCommentedPostByOnePerson
+              .replaceAll('%USER_NAME%', '${usersName.first}');
+        else
+          bodyText = value.notificationBodyCommentedPostBySomePeople
+              .replaceAll('%USER_NAME%', '${usersName.last}')
+              .replaceAll(
+                  '%PEOPLE_AMOUNT%', '${(usersName.length - 1).toString()}');
+      }
+    });
+    return bodyText;
+  }
+
+  goToTimelinePost(List<TimelinePost> posts) async{
+    User? user = await getUserData();
     controller?.insertPage(
       TimelineScreenProfileScreen(
         {
@@ -168,6 +177,11 @@ class PushNotificationBackground {
 
   getPost(String id) async {
     var post = await postsRepository.getPostById(id);
-    goToTimelinePost([post].toList());
+    await goToTimelinePost([post].toList());
+  }
+
+  getUserData() async{
+    bool loggedIn = await userRepository.getUserIsLoggedIn();
+    if (loggedIn) return await userRepository.getCurrentUser();
   }
 }
