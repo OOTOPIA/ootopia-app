253e0d7f51080db1b8b54500a7d01b37001e582b
PauloVOFonseca <paulovitoradsifba@gmai.com>
2022-04-01 15:31:12 -0300

    feat: separate widgets

--- lib/screens/timeline/components/post_timeline_component.dart
+++ lib/screens/timeline/components/post_timeline_component.dart
@@ -7,7 +7,6 @@ import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:intl/intl.dart';
 import 'package:ootopia_app/bloc/post/post_bloc.dart';
 import 'package:ootopia_app/data/models/timeline/like_post_result_model.dart';
-import 'package:ootopia_app/data/models/timeline/media_model.dart';
 import 'package:ootopia_app/data/models/timeline/timeline_post_model.dart';
 import 'package:ootopia_app/data/models/users/user_model.dart';
 import 'package:ootopia_app/data/repositories/wallet_transfers_repository.dart';
@@ -18,6 +17,8 @@ import 'package:ootopia_app/screens/components/popup_menu_post.dart';
 import 'package:ootopia_app/screens/profile_screen/profile_screen.dart';
 import 'package:ootopia_app/screens/timeline/components/comments/comment_screen.dart';
 import 'package:ootopia_app/screens/timeline/components/custom_snackbar_widget.dart';
+import 'package:ootopia_app/screens/timeline/components/header_post_component.dart';
+import 'package:ootopia_app/screens/timeline/components/multiple_medias_component.dart';
 import 'package:ootopia_app/screens/timeline/components/post_timeline_component_controller.dart';
 import 'package:ootopia_app/screens/timeline/components/post_timeline_controller.dart';
 import 'package:ootopia_app/screens/timeline/timeline_store.dart';
@@ -199,61 +200,12 @@ class _PhotoTimelineState extends State<PhotoTimeline> with SecureStoreMixin {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              GestureDetector(
-                onTap: () => _goToProfile(),
-                child: Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(
-                        top: 6.0,
-                        right: 6.0,
-                        bottom: 6.0,
-                      ),
-                      child: this.post.photoUrl != null
-                          ? CircleAvatar(
-                              backgroundImage:
-                                  NetworkImage("${this.post.photoUrl}"),
-                              radius: 16,
-                            )
-                          : CircleAvatar(
-                              backgroundImage:
-                                  AssetImage("assets/icons/user.png"),
-                              radius: 16,
-                            ),
-                    ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width / 1.5,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            this.post.username,
-                            overflow: TextOverflow.ellipsis,
-                            textAlign: TextAlign.start,
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Visibility(
-                            visible: (this.post.city != null &&
-                                    this.post.city!.isNotEmpty) ||
-                                (this.post.state != null &&
-                                    this.post.state!.isNotEmpty),
-                            child: Text(
-                              '${this.post.city}' +
-                                  (this.post.state != null &&
-                                          this.post.state!.isNotEmpty
-                                      ? ', ${this.post.state}'
-                                      : ''),
-                              textAlign: TextAlign.start,
-                              style: TextStyle(fontSize: 12),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+              HeaderPostComponent(
+                username: this.post.username,
+                photoUrl: this.post.photoUrl,
+                city: this.post.city,
+                state: this.post.state,
+                goToProfile: _goToProfile,
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
@@ -454,26 +406,6 @@ class _PhotoTimelineState extends State<PhotoTimeline> with SecureStoreMixin {
                   ),
                 ),
               ),
-              if (this.post.type == 'gallery' &&
-                  this.post.medias!.length > 1 &&
-                  showPosition)
-                Positioned(
-                  right: 23,
-                  top: 22,
-                  child: Container(
-                    height: 20,
-                    width: 35,
-                    child: Center(
-                      child: Text(
-                        '${mediaPosition + 1}/${this.post.medias!.length}',
-                      ),
-                    ),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                    ),
-                  ),
-                ),
             ],
           ),
           if (this.post.type == 'gallery' && this.post.medias!.length > 1) ...[
@@ -901,46 +833,11 @@ class _PhotoTimelineState extends State<PhotoTimeline> with SecureStoreMixin {
         onDoubleTapVideo: () => this._likePost(false, true),
       );
     } else {
-      return SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.width,
-        child: PageView.builder(
-          itemCount: this.post.medias!.length,
-          pageSnapping: true,
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, pagePosition) {
-            return buildMediaRow(this.post.medias![pagePosition]);
-          },
-          onPageChanged: (value) {
-            setState(() {
-              mediaPosition = value;
-              showPosition = true;
-              Future.delayed(Duration(seconds: 2), () {
-                setState(() {
-                  showPosition = false;
-                });
-              });
-            });
-          },
-        ),
-      );
-    }
-  }
-
-  buildMediaRow(Media media) {
-    if (media.type == "image") {
-      return ImagePostTimeline(
-        image: media.mediaUrl as String,
-        onDoubleTapVideo: () => this._likePost(false, true),
-      );
-    } else if (media.type == "video") {
-      return FlickMultiPlayer(
-        userId: (user != null ? user!.id : null),
-        postId: this.post.id,
-        url: media.mediaUrl!,
+      return MultipleMediasComponent(
+        post: post,
+        user: this.user,
         flickMultiManager: widget.flickMultiManager,
-        image: media.thumbUrl!,
-        onDoubleTapVideo: () => this._likePost(false, true),
+        likePost: this._likePost,
       );
     }
   }
@@ -952,8 +849,9 @@ class _PhotoTimelineState extends State<PhotoTimeline> with SecureStoreMixin {
         width: 8,
         height: 8,
         decoration: BoxDecoration(
-            color: mediaPosition == index ? Colors.blue[900] : Colors.black26,
-            shape: BoxShape.circle),
+          color: mediaPosition == index ? Colors.blue[900] : Colors.black26,
+          shape: BoxShape.circle,
+        ),
       );
     });
   }
