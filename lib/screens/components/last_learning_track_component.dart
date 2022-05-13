4036dbcdfd10300e0fa2dc0a655afcb709cd5c78
arielsam567 <Saidemim112104>
2022-05-13 19:11:57 -0300

    refactored page

--- lib/screens/components/last_learning_track_component.dart
+++ lib/screens/components/last_learning_track_component.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -25,18 +23,11 @@ class _LastLearningTrackComponentsState
   SmartPageController controller = SmartPageController.getInstance();
   final currencyFormatter = NumberFormat('#,##0.00', 'ID');
   late AuthStore authStore;
-  bool hasError = false;
   @override
   void initState() {
     super.initState();
     Future.delayed(Duration.zero, () async {
-      learningTracksStore
-          .lastLearningTracks(locale: Platform.localeName)
-          .onError((error, stackTrace) {
-        setState(() {
-          hasError = true;
-        });
-      });
+      learningTracksStore.getLastLearningTracks();
     });
   }
 
@@ -44,7 +35,7 @@ class _LastLearningTrackComponentsState
   Widget build(BuildContext context) {
     authStore = Provider.of<AuthStore>(context);
     return Observer(builder: (_) {
-      if (learningTracksStore.getLastLearningTracks == null) {
+      if (learningTracksStore.lastLearningTracks == null) {
         return Container();
       } else {
         return Container(
@@ -116,9 +107,9 @@ class _LastLearningTrackComponentsState
                     onTap: () {
                       controller.insertPage(ViewLearningTracksScreen({
                         'list_chapters':
-                            learningTracksStore.getLastLearningTracks!.chapters,
+                            learningTracksStore.lastLearningTracks!.chapters,
                         'learning_tracks':
-                            learningTracksStore.getLastLearningTracks,
+                            learningTracksStore.lastLearningTracks,
                       }));
                     },
                     child: Row(
@@ -129,18 +120,18 @@ class _LastLearningTrackComponentsState
                           child: ClipRRect(
                             borderRadius: BorderRadius.all(Radius.circular(10)),
                             child: learningTracksStore
-                                    .getLastLearningTracks!.imageUrl
+                                    .lastLearningTracks!.imageUrl
                                     .contains('.svg')
                                 ? SvgPicture.network(
                                     learningTracksStore
-                                        .getLastLearningTracks!.imageUrl,
+                                        .lastLearningTracks!.imageUrl,
                                     width: 52,
                                     height: 52,
                                     fit: BoxFit.cover,
                                   )
                                 : Image.network(
                                     learningTracksStore
-                                        .getLastLearningTracks!.imageUrl,
+                                        .lastLearningTracks!.imageUrl,
                                     width: 52,
                                     height: 52,
                                     fit: BoxFit.cover,
@@ -157,7 +148,7 @@ class _LastLearningTrackComponentsState
                                 alignment: Alignment.centerLeft,
                                 child: Text(
                                   learningTracksStore
-                                      .getLastLearningTracks!.title,
+                                      .lastLearningTracks!.title,
                                   style: TextStyle(
                                     fontSize: 16,
                                     fontWeight: FontWeight.w400,
@@ -169,7 +160,7 @@ class _LastLearningTrackComponentsState
                                 children: [
                                   Text(
                                     learningTracksStore
-                                        .getLastLearningTracks!.time,
+                                        .lastLearningTracks!.time,
                                     style: TextStyle(
                                       fontSize: 14,
                                       fontWeight: FontWeight.w400,
@@ -196,7 +187,7 @@ class _LastLearningTrackComponentsState
                                     width: 8,
                                   ),
                                   Text(
-                                    '${AppLocalizations.of(context)!.receive} ${currencyFormatter.format(learningTracksStore.getLastLearningTracks!.ooz)}',
+                                    '${AppLocalizations.of(context)!.receive} ${currencyFormatter.format(learningTracksStore.lastLearningTracks!.ooz)}',
                                     style: TextStyle(
                                       fontSize: 14,
                                       fontWeight: FontWeight.w400,
