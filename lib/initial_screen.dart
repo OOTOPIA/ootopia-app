c4d1e760c65a57183943ee079ded1de78908309d
arielsam567 <Saidemim112104>
2022-05-18 16:50:03 -0300

    remove unused code

--- lib/initial_screen.dart
+++ lib/initial_screen.dart
@@ -3,11 +3,9 @@ import 'package:ootopia_app/screens/home/components/home_store.dart';
 import 'package:ootopia_app/screens/home/home_screen.dart';
 import 'package:ootopia_app/screens/splash/splash_screen.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 // ignore: must_be_immutable
 class InitialScreen extends StatelessWidget {
-  SharedPreferences? sharedPreferences;
   HomeStore? homeStore;
 
   @override
